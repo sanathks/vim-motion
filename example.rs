92218145e190fd7388a8673d242a22d21e2db2cf
Sanath Samarasinghe <sanath.s@outlook.com>
2024-05-01 18:22:32 +0200

    add sample comments

--- example.rs
+++ example.rs
@@ -1,15 +1,18 @@
+// Press `shift + g` to move to the end of the file
 trait AcademicEntity {
     fn name(&self) -> &str;
     fn print_details(&self);
 }
-
+// press `19j` to move 19 lines down
 impl AcademicEntity for Student {
     fn name(&self) -> &str {
         &self.name
     }
     fn print_details(&self) {
+        // Press `w` to move to next word, and continue pressing `w` to move to next words
         println!("Student Name: {}", self.name);
         println!("Grades: {:?}", self.grades);
+        //Move to cursor to letter `A` in Average and press `d2w` to delete `Average Grade`
         println!("Average Grade: {:.2}", self.average_grade());
     }
 }
@@ -19,23 +22,24 @@ struct Student {
 }
 
 impl Student {
+    // Move to next line and press `ci(` to change parameters
     fn new(name: &str, grades: Vec<u32>) -> Self {
         Self {
             name: String::from(name),
             grades,
         }
     }
+    // Press `2j` to move the cursor 2 line down then `ci{` to change the block
     fn average_grade(&self) -> f64 {
         let sum: u32 = self.grades.iter().sum();
         let count = self.grades.len() as f64;
         sum as f64 / count
     }
 }
-
+// Press `j` move to next line and press `f{` to move to `{` then press `%` to move to the matching `}`
 fn main() {
-    let alice = Student::new("Alice", vec![90, 85, 88]);
     let bob = Student::new("Bob", vec![78, 80, 85, 90]);
-    let charlie = Student::new("Charlie", vec![95, 92, 98]);
+    let charlie = Student::new("Charlie", vec![95, 95, 119]);
 
     alice.print_details();
     println!();
@@ -43,4 +47,4 @@ fn main() {
     println!();
     charlie.print_details();
 }
-
+// Press `gg` to move to the top of the file
